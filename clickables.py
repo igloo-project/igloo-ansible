260a8b2c083f962c1633ee63f16bb85b20e63382
Laurent Almeras <laurent.almeras@kobalt-si.fr>
2018-11-27 17:41:15 +0100

    updated full playbook path; removed defaults
    defaults are now computed in igloo-ansible-playbooks git submodule

--- clickables.py
+++ clickables.py
@@ -13,6 +13,7 @@ from ruamel.yaml import YAML
 from clickable.utils import PathResolver
 import clickable.coloredlogs
 import clickable_ansible
+import clickable_igloo
 
 
 clickable.coloredlogs.bootstrap()
@@ -23,6 +24,9 @@ logger = logging.getLogger('stdout.clickable')
 @click.group()
 @click.pass_context
 def igloo_ansible(ctx):
+    """
+    Deployment or development tasks
+    """
     ctx.obj = {}
     ctx.obj['path_resolver'] = clickable.utils.PathResolver(sys.modules[__name__])
     ctx.obj['project_root'] = os.path.dirname(__file__)
@@ -35,12 +39,13 @@ def igloo_ansible(ctx):
     logger.debug('loaded configuration: \n{}'.format(pprint.pformat(ctx.obj)))
     # loaded from config
     ctx.obj['virtualenv_path'] = ctx.obj['ansible']['virtualenv']['path']
-    """
-    Deployment or development tasks
-    """
-    pass
+    igloo_ansible_playbooks = os.path.join(ctx.obj['project_root'], 'igloo-ansible-playbooks')
+    clickable_igloo.symlink_folders(igloo_ansible_playbooks, ctx.obj['project_root'])
+
 
 full = clickable_ansible.run_playbook_task(igloo_ansible, 'full',
-        'playbooks/igloo-ansible/full.yml',
+        'playbooks/igloo-playbooks/full.yml',
         short_help="Igloo full deployment",
-        help="""Deploy a full Igloo stack.""")
+        help="""Deploy a full Igloo stack.""",
+        # allow customization
+        common_hosts="igloo.vagrant")
