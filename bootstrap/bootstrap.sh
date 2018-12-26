a07e8ea596ff399daa586761898292e7d5789dbc
Laurent Almeras <laurent.almeras@kobalt-si.fr>
2018-12-26 15:20:34 +0100

    bootstrap: configure poetry before install

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -2,10 +2,13 @@
 
 THIS_SCRIPT="$( realpath "$0" )"
 THIS_PATH="$( dirname "$THIS_SCRIPT" )"
+
 DEFAULT_BOOTSTRAP_NAME="$( basename "$( dirname "$THIS_PATH" )" )"
 BOOTSTRAP_NAME="${BOOTSTRAP_NAME:=${DEFAULT_BOOTSTRAP_NAME}}"
-BOOTSTRAP_PY="https://raw.githubusercontent.com/lalmeras/clickable/ft-ansible/bootstrap/clickable_bootstrap/bootstrap.py"
+
+BOOTSTRAP_BRANCH=${BOOTSTRAP_BRANCH:=ft-ansible}
+BOOTSTRAP_PY="https://raw.githubusercontent.com/lalmeras/clickable/${BOOTSTRAP_BRANCH}/bootstrap/clickable_bootstrap/bootstrap.py"
 
 # environment.yml and pyproject.toml must me in cwd
 cd "${THIS_PATH}"
-wget -q -O - "${BOOTSTRAP_PY}" | BOOTSTRAP_COMMAND="poetry install" python - --name "$BOOTSTRAP_NAME"
+wget -q -O - "${BOOTSTRAP_PY}" | BOOTSTRAP_COMMAND="poetry config settings.virtualenvs.create false && poetry install" python - --name "$BOOTSTRAP_NAME"
